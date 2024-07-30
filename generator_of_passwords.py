5e767cd15d7c0c8fbfd005bb5c58c856b644ebaf
georgeiiister <georgeiiister@gmail.com>
2024-07-31 00:08:38 +0300

    first commit of project generator_of_passwords

---
+++ generator_of_passwords.py
@@ -0,0 +1,21 @@
+import string
+import random
+
+
+def get_password(count_digits: int
+                 , count_ascii_letters: int
+                 , case_sensitive=False) -> str:
+
+    password = ''.join(random.sample(string.digits, count_digits))
+    letters = string.ascii_lowercase
+
+    if case_sensitive:
+        letters += string.ascii_uppercase
+
+    password += ''.join(random.sample(letters, count_ascii_letters))
+    password = ''.join(random.sample(password,len(password)))
+
+    return password
+
+if __name__=='__main__':
+    pass
