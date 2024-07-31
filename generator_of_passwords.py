3c7c55ac1d31e2ccbf1c60fdd4ee980e29b24150
georgeiiister <georgeiiister@gmail.com>
2024-07-31 12:00:25 +0300

    reformat file on PEP8

--- generator_of_passwords.py
+++ generator_of_passwords.py
@@ -5,7 +5,6 @@ import random
 def get_password(count_digits: int
                  , count_ascii_letters: int
                  , case_sensitive=False) -> str:
-
     password = ''.join(random.sample(string.digits, count_digits))
     letters = string.ascii_lowercase
 
@@ -13,9 +12,10 @@ def get_password(count_digits: int
         letters += string.ascii_uppercase
 
     password += ''.join(random.sample(letters, count_ascii_letters))
-    password = ''.join(random.sample(password,len(password)))
+    password = ''.join(random.sample(password, len(password)))
 
     return password
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     pass
